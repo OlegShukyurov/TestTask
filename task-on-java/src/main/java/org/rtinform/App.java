0956467c11015e08928d41e30c674199ccacb253
shukyurov_oleg <oshukiurov@madela.dev>
2024-03-22 21:51:22 +0300

    Возвращение в исходное состояние

--- task-on-java/src/main/java/org/rtinform/App.java
+++ task-on-java/src/main/java/org/rtinform/App.java
@@ -6,7 +6,5 @@ package org.rtinform;
 public class App {
 
     public static void main( String[] args ) {
-        System.out.println("USER1 начинает работать и открывает МР");
-        System.out.println("USER2 начинает работать в той же строчке что и USER1 успешно мержит в мейн");
     }
 }
