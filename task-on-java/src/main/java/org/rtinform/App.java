2c3646a70f25cfa6e5b01cefd046d7fe5b37d38d
shukyurov_oleg <oshukiurov@madela.dev>
2024-03-22 21:06:26 +0300

    USER1 начинает разрабатывать фичу, открывает МР

--- task-on-java/src/main/java/org/rtinform/App.java
+++ task-on-java/src/main/java/org/rtinform/App.java
@@ -6,6 +6,7 @@ package org.rtinform;
 public class App {
 
     public static void main( String[] args ) {
+        System.out.println("USER1 начинает разрабатывать и открывает МР");
         System.out.println("USER2 добавляет на место USER1 код открывает МР и успешно мержит в мейн");
     }
 }
