68998b0d014e82d11a6ec04d58c2d2a029378dda
shukyurov_oleg <oshukiurov@madela.dev>
2024-03-22 17:51:25 +0300

    User 1 впушил свои изменения

--- task-on-java/src/main/java/org/rtinform/App.java
+++ task-on-java/src/main/java/org/rtinform/App.java
@@ -7,6 +7,6 @@ public class App {
 
     public static void main( String[] args ) {
         System.out.println("USER2 впушил изменения");
-        System.out.println("USER2 удачно впушил изменения и влил в дев\\мейн");
+        System.out.println("USER1 впушил свои изменения");
     }
 }
