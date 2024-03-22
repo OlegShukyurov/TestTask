b9449a9646e6e7b73e25454276821ce0d66304dd
shukyurov_oleg <oshukiurov@madela.dev>
2024-03-22 17:10:37 +0300

    123

--- task-on-java/src/main/java/org/rtinform/App.java
+++ task-on-java/src/main/java/org/rtinform/App.java
@@ -7,5 +7,7 @@ public class App {
 
     public static void main( String[] args ) {
         System.out.println("USER2 впушил изменения");
+        System.out.println("USER5 впушил изменения");
+        System.out.println("USER2 удачно впушил изменения и влил в дев\\мейн");
     }
 }
